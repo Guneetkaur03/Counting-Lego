bccfc5512cc669f49e5d4410398dce2403d968c6
Guneet kaur <guneet.kaur.15cse@bml.edu.in>
2018-11-15 22:14:52 +0530

    Python code

---
+++ count lego.py
@@ -0,0 +1,25 @@
+
+import cv2
+import numpy as np
+
+# Read in the image in grayscale
+img = cv2.imread('30-legos.jpg', cv2.IMREAD_GRAYSCALE)
+
+# Determine which openCV version were using
+if cv2.__version__.startswith('2.'):
+    detector = cv2.SimpleBlobDetector()
+else:
+    detector = cv2.SimpleBlobDetector_create()
+
+# Detect the blobs in the image
+keypoints = detector.detect(img)
+print(len(keypoints))
+
+# Draw detected keypoints as green circles
+imgKeyPoints = cv2.drawKeypoints(img, keypoints, np.array([]), (0,255,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+# Display found keypoints
+cv2.imshow("Keypoints", imgKeyPoints)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
